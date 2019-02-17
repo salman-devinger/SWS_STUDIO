09f144c5e3d957d847558fc9d6e8e7da1ea9e688
salman khan <salman.khan@rsgmedia.com>
2019-02-17 15:26:54 +0530

    Changes

---
+++ portal/login_validate0.php
@@ -0,0 +1,11 @@
+<?php
+
+// Initialize the session
+session_start();
+$_SESSION['url'] = $_SERVER['REQUEST_URI'];
+// If session variable is not set it will redirect to login page
+if(!isset($_SESSION['user']) || empty($_SESSION['user'])){
+  header("location: ../login.php");
+  exit;
+}
+?>
